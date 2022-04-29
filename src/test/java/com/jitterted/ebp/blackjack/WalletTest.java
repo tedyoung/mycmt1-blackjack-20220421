d87f4673db3a7428652e180032849bb1e9505dc7
Ted M. Young <tedyoung@gmail.com>
2022-04-29 14:12:06 -0700

    TDD: add money twice has correct balance

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -42,4 +42,15 @@ public class WalletTest {
                 .isEqualTo(10);
     }
 
+    @Test
+    public void walletAdd7And8ThenBalanceIs15() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(7);
+        wallet.addMoney(8);
+
+        assertThat(wallet.balance())
+                .isEqualTo(7 + 8);
+    }
+
 }
